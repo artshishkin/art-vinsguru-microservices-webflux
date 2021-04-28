951d1686c15c4136a19e27b9af956b79d579f75e
Art <d.art.shishkin@gmail.com>
2021-04-28 14:45:12 +0300

    42. GET Mono Endpoint - subscribe (#4)

--- webflux-demo/src/test/java/net/shyshkin/study/webflux/webfluxdemo/Section5WebClient/Lec41_WebClient_Bean_Test.java
+++ webflux-demo/src/test/java/net/shyshkin/study/webflux/webfluxdemo/Section5WebClient/Lec41_WebClient_Bean_Test.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Lec41_WebClient_Bean_Test extends BaseTest {
@@ -46,5 +50,32 @@ public class Lec41_WebClient_Bean_Test extends BaseTest {
 
         //then
         log.debug("[{}] {}", responseEntity.getStatusCode(), responseEntity.getBody());
+    }
+
+    @Test
+    void findSquare_mono_toEntity() throws InterruptedException {
+
+        //given
+        int input = 16;
+        CountDownLatch latch = new CountDownLatch(1);
+
+        //when
+        Mono<ResponseEntity<Response>> responseEntityMono = webClient
+                .get()
+                .uri("/reactive-math/square/{input}", input)
+                .retrieve()
+                .toEntity(Response.class);
+
+        //then
+        responseEntityMono
+                .subscribe(
+                        responseEntity -> log.debug("[{}] {}", responseEntity.getStatusCode(), responseEntity.getBody()),
+                        ex -> log.error("{}:{}", ex.getClass().getName(), ex.getMessage()),
+                        () -> {
+                            latch.countDown();
+                            log.debug("Completed");
+                        });
+
+        latch.await(1, TimeUnit.SECONDS);
     }
 }
