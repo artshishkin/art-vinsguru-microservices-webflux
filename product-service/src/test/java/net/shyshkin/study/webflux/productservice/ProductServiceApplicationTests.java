81284f473339bd9e40774f66895f03f130835ef0
Art <d.art.shishkin@gmail.com>
2021-04-29 08:21:27 +0300

    60. Project Setup (#5 Section 6: Spring Data Reactive MongoDB)

---
+++ product-service/src/test/java/net/shyshkin/study/webflux/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.webflux.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
