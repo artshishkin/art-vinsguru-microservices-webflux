4912420bebb9f9f491853eb60041870ce4f4e978
Art <d.art.shishkin@gmail.com>
2021-04-29 23:59:34 +0300

    90. [User-Service] - Transaction DTO Util (#6)

--- user-service/src/main/java/net/shyshkin/study/webflux/userservice/mapper/EntityMapper.java
+++ user-service/src/main/java/net/shyshkin/study/webflux/userservice/mapper/EntityMapper.java
@@ -1,14 +1,28 @@
 package net.shyshkin.study.webflux.userservice.mapper;
 
+import net.shyshkin.study.webflux.userservice.dto.TransactionRequestDto;
+import net.shyshkin.study.webflux.userservice.dto.TransactionResponseDto;
+import net.shyshkin.study.webflux.userservice.dto.TransactionStatus;
 import net.shyshkin.study.webflux.userservice.dto.UserDto;
 import net.shyshkin.study.webflux.userservice.entity.User;
+import net.shyshkin.study.webflux.userservice.entity.UserTransaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+import java.time.LocalDateTime;
+
+@Mapper(imports = LocalDateTime.class)
 public interface EntityMapper {
 
     User toEntity(UserDto dto);
 
     UserDto toDto(User user);
+
+    @Mapping(target = "timestamp", expression = "java(LocalDateTime.now())")
+    UserTransaction toEntity(TransactionRequestDto requestDto);
+
+    TransactionResponseDto toDto(UserTransaction userTransaction, TransactionStatus status);
+
+    TransactionResponseDto toDto(TransactionRequestDto requestDto, TransactionStatus status);
 
 }
