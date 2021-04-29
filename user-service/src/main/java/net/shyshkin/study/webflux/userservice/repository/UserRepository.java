d186beb6f9e71b618709329262a2e342266fb3a6
Art <d.art.shishkin@gmail.com>
2021-04-29 23:44:54 +0300

    89. [User-Service] - User Balance Update - R2DBC with @Query (#6)

--- user-service/src/main/java/net/shyshkin/study/webflux/userservice/repository/UserRepository.java
+++ user-service/src/main/java/net/shyshkin/study/webflux/userservice/repository/UserRepository.java
@@ -1,7 +1,18 @@
 package net.shyshkin.study.webflux.userservice.repository;
 
 import net.shyshkin.study.webflux.userservice.entity.User;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveSortingRepository<User, Integer> {
+
+    @Modifying
+    @Query(
+            value = "update users " +
+                    "set balance = balance + :amount " +
+                    "where id = :userId and balance >= :amount"
+    )
+    Mono<Boolean> updateUserBalance(Integer userId, Integer amount);
 }
