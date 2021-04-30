f690c47845b815d54d2dd8280eee54dc14168ce9
Art <d.art.shishkin@gmail.com>
2021-04-30 15:57:11 +0300

    111. [Order-Service] - User Client (#7)

---
+++ order-service/order-service-server/src/main/java/net/shyshkin/study/webflux/orderservice/client/UserClient.java
@@ -0,0 +1,26 @@
+package net.shyshkin.study.webflux.orderservice.client;
+
+import net.shyshkin.study.webflux.userservice.dto.TransactionRequestDto;
+import net.shyshkin.study.webflux.userservice.dto.TransactionResponseDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class UserClient {
+
+    private final WebClient webClient;
+
+    public UserClient(WebClient webClient, @Value("${user-service.url}") String url) {
+        this.webClient = webClient.mutate().baseUrl(url).build();
+    }
+
+    public Mono<TransactionResponseDto> createTransaction(TransactionRequestDto requestDto) {
+        return webClient.post()
+                .uri("transactions")
+                .bodyValue(requestDto)
+                .retrieve()
+                .bodyToMono(TransactionResponseDto.class);
+    }
+}
