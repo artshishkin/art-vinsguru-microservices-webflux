7422255361a0c029fbf563a6a6b673a24bbe7756
Art <d.art.shishkin@gmail.com>
2021-05-02 10:20:53 +0300

    128. Exposing Event Stream (#8)

---
+++ product-service/product-service-server/src/main/java/net/shyshkin/study/webflux/productservice/controller/ProductStreamController.java
@@ -0,0 +1,22 @@
+package net.shyshkin.study.webflux.productservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.webflux.productservice.dto.ProductDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("products")
+@RequiredArgsConstructor
+public class ProductStreamController {
+
+    private final Flux<ProductDto> productBroadcast;
+
+    @GetMapping(value = "broadcast", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ProductDto> getProductUpdates() {
+        return productBroadcast;
+    }
+}
