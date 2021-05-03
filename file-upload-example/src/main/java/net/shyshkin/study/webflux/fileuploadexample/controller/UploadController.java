18cf8253af88b624a33e97e13385daf14fb2a743
Art <d.art.shishkin@gmail.com>
2021-05-03 18:22:25 +0300

    137. Upload Single File Endpoint (#11)

---
+++ file-upload-example/src/main/java/net/shyshkin/study/webflux/fileuploadexample/controller/UploadController.java
@@ -0,0 +1,43 @@
+package net.shyshkin.study.webflux.fileuploadexample.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Slf4j
+@RestController
+@RequestMapping("upload")
+public class UploadController {
+
+    public static final Path basePath = Paths.get("target/upload/").toAbsolutePath();
+
+    @PostConstruct
+    private void init() {
+        try {
+            Files.createDirectories(basePath);
+        } catch (IOException e) {
+            log.error("Error in directories creation", e);
+        }
+    }
+
+
+    @PostMapping("file/single")
+    public Mono<Void> uploadSingleFile(@RequestPart("user-name") String name,
+                                       @RequestPart("fileToUpload") Mono<FilePart> filePartMono) {
+
+        log.debug("user: {} tries to upload to {}", name, basePath);
+        return filePartMono
+                .doOnNext(filePart -> log.debug("Received file: {}", filePart.filename()))
+                .flatMap(filePart -> filePart.transferTo(basePath.resolve(filePart.filename())));
+    }
+}
---
+++ file-upload-example/src/test/java/net/shyshkin/study/webflux/fileuploadexample/controller/UploadControllerTest.java
@@ -0,0 +1,148 @@
+package net.shyshkin.study.webflux.fileuploadexample.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class UploadControllerTest {
+
+    @Autowired
+    WebTestClient webClient;
+
+    @Test
+    void uploadSingleFile_fromClassPathResource() throws IOException {
+
+        //given
+        String filename = "file1.txt";
+        String fileContent = "file1 content";
+
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder
+                .part("fileToUpload", new ClassPathResource("/files/file1.txt"))
+                .contentType(MediaType.MULTIPART_FORM_DATA);
+        multipartBodyBuilder.part("user-name", "Kate Shyshkina");
+        MultiValueMap<String, HttpEntity<?>> multipartData = multipartBodyBuilder.build();
+
+        //when
+        webClient.post()
+                .uri("/upload/file/single")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(multipartData))
+                .exchange()
+
+                //then
+                .expectStatus().isOk()
+                .expectBody().isEmpty();
+
+        String content = Files.readString(UploadController.basePath.resolve(filename));
+        assertThat(content).isEqualTo(fileContent);
+
+    }
+
+    @Test
+    void uploadSingleFile_fromByteArrayResource() throws IOException {
+
+        //given
+        String filename = "byteArrayResource.txt";
+        String fileContent = "Byte Array Resource content";
+
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder
+                .part("fileToUpload",
+                        new ByteArrayResource(fileContent.getBytes(), "byteArray description"),
+                        MediaType.MULTIPART_FORM_DATA)
+                .filename(filename);
+        multipartBodyBuilder.part("user-name", "Kate Shyshkina");
+        MultiValueMap<String, HttpEntity<?>> multipartData = multipartBodyBuilder.build();
+
+        //when
+        webClient.post()
+                .uri("/upload/file/single")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(multipartData))
+                .exchange()
+
+                //then
+                .expectStatus().isOk()
+                .expectBody().isEmpty();
+
+        String content = Files.readString(UploadController.basePath.resolve(filename));
+        assertThat(content).isEqualTo(fileContent);
+    }
+
+    @Test
+    void uploadSingleFile_fromByteArray() throws IOException {
+
+        //given
+        String filename = "byteArray.txt";
+        String fileContent = "Byte Array content";
+
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder
+                .part("fileToUpload",
+                        fileContent.getBytes(),
+                        MediaType.MULTIPART_FORM_DATA)
+                .filename(filename);
+        multipartBodyBuilder.part("user-name", "Kate Shyshkina");
+        MultiValueMap<String, HttpEntity<?>> multipartData = multipartBodyBuilder.build();
+
+        //when
+        webClient.post()
+                .uri("/upload/file/single")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(multipartData))
+                .exchange()
+
+                //then
+                .expectStatus().isOk()
+                .expectBody().isEmpty();
+
+        String content = Files.readString(UploadController.basePath.resolve(filename));
+        assertThat(content).isEqualTo(fileContent);
+    }
+
+    @Test
+    void uploadSingleFile_fromString() throws IOException {
+
+        //given
+        String filename = "fromString.txt";
+        String fileContent = "String content";
+
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder
+                .part("fileToUpload", fileContent)
+                .filename(filename);
+        multipartBodyBuilder.part("user-name", "Kate Shyshkina");
+        MultiValueMap<String, HttpEntity<?>> multipartData = multipartBodyBuilder.build();
+
+        //when
+        webClient.post()
+                .uri("/upload/file/single")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(multipartData))
+                .exchange()
+
+                //then
+                .expectStatus().isOk()
+                .expectBody().isEmpty();
+
+        String content = Files.readString(UploadController.basePath.resolve(filename));
+        assertThat(content).isEqualTo(fileContent);
+    }
+}
