f63ef3d477a61a01506ced04b9a15b3fb2148c2e
Art <d.art.shishkin@gmail.com>
2021-11-08 13:20:38 +0200

    13.3 CORS (#13)

--- product-service/product-service-server/src/main/java/net/shyshkin/study/webflux/productservice/controller/ProductStreamController.java
+++ product-service/product-service-server/src/main/java/net/shyshkin/study/webflux/productservice/controller/ProductStreamController.java
@@ -3,15 +3,13 @@ package net.shyshkin.study.webflux.productservice.controller;
 import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.webflux.productservice.dto.ProductDto;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("products")
 @RequiredArgsConstructor
+@CrossOrigin
 public class ProductStreamController {
 
     private final Flux<ProductDto> productBroadcast;
